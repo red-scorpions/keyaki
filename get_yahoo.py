d57ed8e86e0e997d7130f094680f6595ca20ee83
nagata_motoki <nagata.motoki@shi-juku.co.jp>
2016-12-04 01:43:31 +0900

    yahooの検索ボタンのクリックには成功した

--- get_yahoo.py
+++ get_yahoo.py
@@ -5,19 +5,19 @@ from selenium import webdriver
 import os
 import time
 driver = webdriver.Chrome('./chromedriver')
-driver.get('https://www.keyakinet.jp/w/')
+driver.get('http://www.yahoo.co.jp/')
 print type(driver)
 print driver
-ele_iframe = driver.find_element_by_name("iframe")
+#ele_iframe = driver.find_element_by_name("iframe")
 time.sleep(5)
-print type(ele_iframe)
-print ele_iframe
+# print type(ele_iframe)
+# print ele_iframe
 # ele_contid = ele_iframe.find_element_by_id("CONTID")
 # print ele_contid
-# apply_button = ele_iframe.find_element_by_xpath("//input[@id='BB0']")
-apply_button = driver.find_element_by_xpath("//input[@id='BB0']")
+submit_button = driver.find_element_by_xpath("//input[@id='srchbtn']")
+#apply_button = ele_iframe.find_element_by_xpath("//input[@id='BB0']")
 # apply_button = ele_iframe.find_element_by_id("BB0")
-apply_button.click()
+submit_button.click()
 
 
 
