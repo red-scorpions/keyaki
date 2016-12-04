0e1b59329d34af3c48c981a0580f4cefa1cd6de9
nagata_motoki <nagata.motoki@shi-juku.co.jp>
2016-12-04 22:42:50 +0900

    メインのコード追加

--- get_lottery.py
+++ get_lottery.py
@@ -1,24 +1,11 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from selenium import webdriver
 import os
-import time
-driver = webdriver.Chrome('./chromedriver')
-driver.get('https://www.keyakinet.jp/w/')
-print type(driver)
-print driver
-ele_iframe = driver.find_element_by_name("iframe")
-time.sleep(5)
-print type(ele_iframe)
-print ele_iframe
-# ele_contid = ele_iframe.find_element_by_id("CONTID")
-# print ele_contid
-# apply_button = ele_iframe.find_element_by_xpath("//input[@id='BB0']")
-apply_button = driver.find_element_by_xpath("//input[@id='BB0']")
-# apply_button = ele_iframe.find_element_by_id("BB0")
-apply_button.click()
+from helper import selenium_helper
+import commands
 
-
-
-
+commands.getoutput("sh set_env.sh")
+driver = selenium_helper.access()
+selenium_helper.login(driver)
+selenium_helper.change_school_and_get_count(driver)
