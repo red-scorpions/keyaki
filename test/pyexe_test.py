439b647e682a820be732fca82347729c92d895c7
永田基樹 <nagata.motoki@shi-juku.co.jp>
2017-03-08 14:59:31 +0900

    Py2exeは試したけどダメだった

--- test/pyexe_test.py
+++ test/pyexe_test.py
@@ -2,10 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import os
-from helper import selenium_helper
 import subprocess
+import platform
+if platform.system() == "Windows":
+    os.system("..\set_env.bat")
+else:
+    subprocess.call(("sh ../set_env.sh"),shell=True)
+import sys
+if platform.system() == "Windows":
+    sys.path.append("..")
+print sys.path
+
+
+from helper import selenium_helper
 from helper import os_helper
 
-subprocess.call(os_helper.change_ps("sh ../set_env.sh"),shell=True)
 driver = selenium_helper.access_yahoo()
 driver.close()
+
